edd95b2c6f5e18c8e10de99b183b110c6fa9b7ea
Partha M <erparthamondal@gmail.com>
2026-08-15 14:30:11 +0530

    Add files via upload

---
+++ data/playlists.ts
@@ -0,0 +1,6 @@
+// Swap this placeholder ID for your real YouTube playlist ID.
+// Get it from a playlist URL: youtube.com/playlist?list=THIS_PART
+export const playlist = {
+  id: "PLACEHOLDER_PLAYLIST_ID",
+  url: "https://music.youtube.com/playlist?list=PLACEHOLDER_PLAYLIST_ID",
+};
