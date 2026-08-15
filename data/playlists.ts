6cc28bb43471997d1303ccfc5e28adeaef2e5e8b
Partha M <erparthamondal@gmail.com>
2026-08-15 21:17:57 +0530

    Update playlists.ts

--- data/playlists.ts
+++ data/playlists.ts
@@ -1,6 +1,6 @@
 // Swap this placeholder ID for your real YouTube playlist ID.
 // Get it from a playlist URL: youtube.com/playlist?list=THIS_PART
 export const playlist = {
-  id: "PLACEHOLDER_PLAYLIST_ID",
+  id: "PLehjuCKQdN-c",
   url: "https://youtube.com/playlist?list=PLehjuCKQdN-c",
 };
