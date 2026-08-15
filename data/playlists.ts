1c0ae07d621d0ab58900516cc33c92d448c74c0e
Partha M <erparthamondal@gmail.com>
2026-08-15 20:38:54 +0530

    Update playlists.ts

--- data/playlists.ts
+++ data/playlists.ts
@@ -2,5 +2,5 @@
 // Get it from a playlist URL: youtube.com/playlist?list=THIS_PART
 export const playlist = {
   id: "PLACEHOLDER_PLAYLIST_ID",
-  url: "https://music.youtube.com/playlist?list=PLACEHOLDER_PLAYLIST_ID",
+  url: "https://music.youtube.com/playlist?list=PLehjuCKQdN-c",
 };
