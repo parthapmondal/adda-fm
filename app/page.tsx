a6de74f2b0ae9bfa4ec49580507d032f94b02af6
Partha M <erparthamondal@gmail.com>
2026-08-16 16:31:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,6 @@
-import Clock from "@/components/Clock";
-import DiscIcon from "@/components/DiscIcon";
-import TriviaStrip from "@/components/Trivia";
-import Player from "@/components/Player";
-import { playlist } from "@/data/playlists";
-import { trivia } from "@/data/trivia";
-import { basePath } from "@/lib/basePath";
+import GenreShell from "@/components/GenreShell";
+import { genres } from "@/data/genres";
 
 export default function Home() {
-  return (
-    <div className="stage">
-      <div
-        className="stage__bg"
-        style={{ backgroundImage: `url(${basePath}/images/bg-main.png)` }}
-      />
-      <div className="stage__scrim" />
-
-      <Clock />
-      <DiscIcon href={playlist.url} />
-
-      <div className="titleblock">
-        <h1 className="titleblock__title">
-          {"\u0986\u09a1\u09cd\u09a1\u09be"} <span className="titleblock__en">FM</span>
-        </h1>
-      </div>
-
-      <TriviaStrip items={trivia} />
-      <Player playlistId={playlist.id} mountId="yt-player" />
-    </div>
-  );
+  return <GenreShell genre={genres[0]} />;
 }
